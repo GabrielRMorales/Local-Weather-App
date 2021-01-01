4391b0eb2be95642d215df3fdb33d1dd030d3573
Gabriel Morales <grmorale@gmail.com>
2020-12-31 22:33:04 -0800

    Hide secret info

--- server/server.js
+++ server/server.js
@@ -5,14 +5,18 @@ import express from "express";
 
 const app=express();
 const __dirname=path.resolve();
-const publicPath = path.join(__dirname,"..", "public");
+//const publicPath = path.join(__dirname,"..", "public");
 const port = process.env.PORT || 3000;
 
-app.use(express.static(publicPath));
+//app.use(express.static(publicPath));
+app.get("/",(req,res)=>{
+  
+    res.sendFile(path.join(__dirname+"/public/index.html"));
+})
 
-app.get("*",(req,res)=>{
+/*app.get("*",(req,res)=>{
     res.sendFile(path.join(publicPath, "index.html"));
-});
+});*/
 
 app.listen(port, ()=>{
     console.log(`server is up on ${port}`);
